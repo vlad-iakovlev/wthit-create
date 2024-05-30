748d4f990752a536b2e01fc6f4f70a6d60aa6046
Vladislav Yakovlev <vlad309523@gmail.com>
2024-05-31 03:30:41 +0400

    chore: init project

---
+++ src/main/java/vladiakovlev/wthitcreate/WTHITCreate.java
@@ -0,0 +1,9 @@
+package vladiakovlev.wthitcreate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WTHITCreate {
+	public static final String MOD_ID = "wthit-create";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+}
