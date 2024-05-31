5c256bd8be68a0f8ed0abd01fc919794b3421f8d
Vladislav Yakovlev <vlad309523@gmail.com>
2024-06-01 02:45:32 +0400

    feat: add copycat provider

--- src/main/java/vladiakovlev/wthitcreate/WTHITCreatePlugin.java
+++ src/main/java/vladiakovlev/wthitcreate/WTHITCreatePlugin.java
@@ -4,6 +4,8 @@ import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IWailaPlugin;
 import vladiakovlev.wthitcreate.provider.BacktankProvider;
 import vladiakovlev.wthitcreate.provider.BlazeBurnerProvider;
+import vladiakovlev.wthitcreate.provider.CopycatProvider;
+import vladiakovlev.wthitcreate.provider.PlacardProvider;
 
 public class WTHITCreatePlugin implements IWailaPlugin {
 
@@ -11,6 +13,8 @@ public class WTHITCreatePlugin implements IWailaPlugin {
 	public void register(IRegistrar registrar) {
 		BacktankProvider.INSTANCE.register(registrar);
 		BlazeBurnerProvider.INSTANCE.register(registrar);
+		CopycatProvider.INSTANCE.register(registrar);
+		PlacardProvider.INSTANCE.register(registrar);
 	}
 
 }
--- src/main/java/vladiakovlev/wthitcreate/provider/CopycatProvider.java
+++ src/main/java/vladiakovlev/wthitcreate/provider/CopycatProvider.java
@@ -1,6 +1,7 @@
 package vladiakovlev.wthitcreate.provider;
 
-import com.simibubi.create.content.decoration.placard.PlacardBlockEntity;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.decoration.copycat.CopycatBlockEntity;
 
 import mcp.mobius.waila.api.IBlockAccessor;
 import mcp.mobius.waila.api.IBlockComponentProvider;
@@ -10,23 +11,23 @@ import mcp.mobius.waila.api.ITooltip;
 import mcp.mobius.waila.api.TooltipPosition;
 import mcp.mobius.waila.api.component.WrappedComponent;
 
-public enum PlacardProvider implements IBlockComponentProvider {
+public enum CopycatProvider implements IBlockComponentProvider {
 	INSTANCE;
 
 	public void register(IRegistrar registrar) {
-		registrar.addComponent(this, TooltipPosition.BODY, PlacardBlockEntity.class);
+		registrar.addComponent(this, TooltipPosition.BODY, CopycatBlockEntity.class);
 	}
 
 	@Override
 	public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
-		var placard = (PlacardBlockEntity) accessor.getBlockEntity();
-		var item = placard.getHeldItem();
+		var copycat = (CopycatBlockEntity) accessor.getBlockEntity();
+		var material = copycat.getMaterial();
 
-		if (item.isEmpty()) {
+		if (material == AllBlocks.COPYCAT_BASE.getDefaultState()) {
 			return;
 		}
 
-		tooltip.addLine(new WrappedComponent(item.getHoverName()));
+		tooltip.addLine(new WrappedComponent(material.getBlock().getName()));
 	}
 
 }
