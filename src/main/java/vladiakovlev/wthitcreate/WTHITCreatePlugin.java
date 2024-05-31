2a89424a6235e7293b75f1e2728f4b6db940b9fe
Vladislav Yakovlev <vlad309523@gmail.com>
2024-06-01 02:20:44 +0400

    fix: enable blaze burner provider

--- src/main/java/vladiakovlev/wthitcreate/WTHITCreatePlugin.java
+++ src/main/java/vladiakovlev/wthitcreate/WTHITCreatePlugin.java
@@ -3,12 +3,14 @@ package vladiakovlev.wthitcreate;
 import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IWailaPlugin;
 import vladiakovlev.wthitcreate.provider.BacktankProvider;
+import vladiakovlev.wthitcreate.provider.BlazeBurnerProvider;
 
 public class WTHITCreatePlugin implements IWailaPlugin {
 
 	@Override
 	public void register(IRegistrar registrar) {
 		BacktankProvider.INSTANCE.register(registrar);
+		BlazeBurnerProvider.INSTANCE.register(registrar);
 	}
 
 }
